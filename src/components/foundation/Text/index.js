57625ba55d4fa7673a4bd2a28a225bf46019d0b3
Giovanni Tonussi <Giovanni.Tonussi@ibm.com>
2021-08-16 18:16:10 -0300

    feat(Text.js): include all variants

--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -3,16 +3,40 @@ import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
 export const TextStyleVariantsMap = {
-  smallestException: css`
-    font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontSize};
-    font-weight: ${({ theme }) => theme.typographyVariants.smallestException.fontWeight};
-    line-height: ${({ theme }) => theme.typographyVariants.smallestException.lineHeight};
+  title: css`
+    font-size: ${({ theme }) => theme.typographyVariants.title.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.title.fontWeight};
+    line-height: ${({ theme }) => theme.typographyVariants.title.lineHeight};
+  `,
+
+  titleXs: css`
+    font-size: ${({ theme }) => theme.typographyVariants.titleXs.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.titleXs.fontWeight};
+    line-height: ${({ theme }) => theme.typographyVariants.titleXs.lineHeight};
+  `,
+
+  subtitle: css`
+    font-size: ${({ theme }) => theme.typographyVariants.subtitle.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.subtitle.fontWeight};
+    line-height: ${({ theme }) => theme.typographyVariants.subtitle.lineHeight};
   `,
 
   paragraph1: css`
     font-size: ${({ theme }) => theme.typographyVariants.paragraph1.fontSize};
     font-weight: ${({ theme }) => theme.typographyVariants.paragraph1.fontWeight};
     line-height: ${({ theme }) => theme.typographyVariants.paragraph1.lineHeight};
+  `,
+
+  paragraph2: css`
+    font-size: ${({ theme }) => theme.typographyVariants.paragraph2.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.paragraph2.fontWeight};
+    line-height: ${({ theme }) => theme.typographyVariants.paragraph2.lineHeight};
+  `,
+  
+  smallestException: css`
+    font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.smallestException.fontWeight};
+    line-height: ${({ theme }) => theme.typographyVariants.smallestException.lineHeight};
   `,
 }
 

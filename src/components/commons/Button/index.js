27bdd74183be61589bc22b59acc6fc4995c448c6
Giovanni Tonussi <Giovanni.Tonussi@ibm.com>
2021-08-18 20:25:56 -0300

    feat(aula 08): implements propToStyle function

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import { TextStyleVariantsMap } from '../../foundation/Text';
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+import { propToStyle } from '../../../theme/utils/propToStyle';
 
 const ButtonGhost = css`
   background: transparent;
@@ -38,5 +39,9 @@ export const Button = styled.button`
       ${TextStyleVariantsMap.paragraph1}
     `,
   })}
+
+  ${propToStyle('margin')}
+
+  ${propToStyle('display')}
 
 `;
--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
+import { propToStyle } from '../../../theme/utils/propToStyle';
 
 export const TextStyleVariantsMap = {
   title: css`
@@ -42,11 +43,13 @@ export const TextStyleVariantsMap = {
 
 const TextBase = styled.span`
   ${({ variant }) => TextStyleVariantsMap[variant]}
+
+  ${propToStyle('textAlign')}
 `;
 
-export default function Text({ tag, variant, children }) {
+export default function Text({ tag, variant, children, ...props }) {
   return (
-    <TextBase as={tag} variant={variant}>
+    <TextBase as={tag} variant={variant} {...props}>
       {children}
     </TextBase>
   )
